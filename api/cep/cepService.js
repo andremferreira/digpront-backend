dfe5194142e2868337353724bcddfd690bd2b5e4
andremferreira <ferreiraandremendes@gmail.com>
2018-06-28 11:35:01 -0300

    CepConfig
    Exluído rotas para delete e update.

--- api/cep/cepService.js
+++ api/cep/cepService.js
@@ -1,6 +1,6 @@
 const Cep = require('./cep')
 
-Cep.methods(['get', 'post', 'put', 'delete'])
+Cep.methods(['get', 'post'])
 Cep.updateOptions({ new: true, runValidators: true })
 
 module.exports = Cep
